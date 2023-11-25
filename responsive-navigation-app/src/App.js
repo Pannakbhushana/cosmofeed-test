4919189493e84ff18bf6935ea0586a64d94e32b6
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-11-25 14:23:40 +0530

    bug fixed

--- responsive-navigation-app/src/App.js
+++ responsive-navigation-app/src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import AllRoutes from './Router/AllRoutes';
-import NavigationMenu from './Components/NavigationMenu';
+import { Link } from 'react-router-dom';
+import styles from "./Style/Navbar.module.css"
+import { useState } from 'react';
 
 
 const App = () => {
+  const [state, setState]=useState(false);
+
+  const handleToggle=()=>{
+    setState(!state);
+  }
   return (
     <div>
-     <NavigationMenu/>
+
+      <div className={styles.navbar}>
+        <div className={styles.menu} >
+          <p><Link to="/">Home</Link></p>
+          <p><Link to="/product">Product</Link></p>
+          <p><Link to="/contact">Contact</Link></p>
+          <p><Link to="/auth">Auth</Link></p>
+        </div>
+
+      <div className={styles.hemberger} onClick={handleToggle} >
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+
+          <div className={styles.dropDown} style={{display:state ? "block":"none"}} >
+            <p><Link to="/">Home</Link></p>
+            <p><Link to="/product">Product</Link></p>
+            <p><Link to="/contact">Contact</Link></p>
+            <p><Link to="/auth">Auth</Link></p>
+          </div>
+
+      </div>
+
+      </div>
+
      <AllRoutes/>
+
     </div>
   );
 };
