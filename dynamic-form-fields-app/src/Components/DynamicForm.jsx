0eab1adb7f5b0ef912dd156191f318309ff48889
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-11-25 16:07:29 +0530

    bug fixed

--- dynamic-form-fields-app/src/Components/DynamicForm.jsx
+++ dynamic-form-fields-app/src/Components/DynamicForm.jsx
@@ -1,64 +1,49 @@
-import React, { useState } from 'react';
 
-const DynamicForm = () => {
-  const [data, setData] = useState([{ value: "" }]);
+import React, { useState } from 'react'
 
-  const handleAddInput = () => {
-    setData([...data, { value: "" }]);
-  };
+function DynamicForm() {
+  const [data, setData]=useState([{value:""}]);
 
-  const handleRemoveInput = (ind) => {
-    const newData = [...data];
-    newData.splice(ind, 1);
+  const handleChange=(i,e)=>{
+    data[i].value=e.target.value;
+    setData(data)
+  }
+
+  const handleSubmit=(e)=>{
+    console.log(data.map((el)=>el.value))
+  }
+
+  const handleAddInput=()=>{
+    setData([...data,{value:""}]);
+  }
+
+  const handleRemove=(ind)=>{
+    const newData=data.filter((el,i)=>{
+      return i!==ind;
+    })
     setData(newData);
-  };
-
-  const handleChange = (i, e) => {
-    const newData = [...data];
-    newData[i].value = e.target.value;
-    setData(newData);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log( data.map((el) => el.value));
-  };
+  }
 
   return (
-    <div style={{
-                    marginTop:"10%",
-                    boxShadow:"rgba(0, 0, 0, 0.35) 0px 5px 15px",
-                    width:"30%",marginLeft:"35%",
-                    padding:"50px",
-                    borderRadius:"5%"
-                    }}>
-
-            <h1>Dynamic Form Fields</h1>
-
-        <form onSubmit={handleSubmit}>
-      {data.map((el, i) => (
-        <div key={i}>
-          <input
-            type="text"
-            placeholder="Enter a value"
-            value={el.value}
-            onChange={(e) => handleChange(i, e)}
-          />
-          <button type="button" onClick={() => handleRemoveInput(i)}>
-            Remove
-          </button>
-          <br />
-          <br />
-        </div>
-        
-      ))}
-      <button type="button" onClick={handleAddInput}>
-        Add Input
-      </button>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <h1>Welcom to home page</h1>
+      <br />
+      {
+        data.map((el,i)=>{
+          return <div key={i}>
+            <input type="text" placeholder='Add value' onChange={(e)=>{
+              handleChange(i,e)
+            }} />
+            <button onClick={()=>{
+              handleRemove(i)
+            }}>Remove</button>
+          </div>
+        })
+      }
+      <button onClick={handleSubmit} >Submit</button>
+      <button onClick={handleAddInput}>Add Input</button>
     </div>
-  );
-};
+  )
+}
 
-export default DynamicForm;
+export default DynamicForm
